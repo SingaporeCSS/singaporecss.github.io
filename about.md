a38f3b6b6a0dc46684d67e6bbe5caf6c00e07f8a
huijing <kakyou_tensai@yahoo.com>
2016-03-02 16:50:45 +0800

    Initial commit of site files (severely WIP)

---
+++ about.md
@@ -0,0 +1,11 @@
+---
+layout: page
+title: About
+permalink: /about/
+---
+
+Developers love meet-ups. Almost every programming language has its own local meet-up. Almost. There wasn't a CSS-specific meet-up in Singapore at the time, and usually talks related to CSS latched onto other meet-ups like [Talk.JS](http://www.meetup.com/Singapore-JS/) or [Front End Developers Singapore](http://feds.strikingly.com/).
+
+Talk.CSS came into being on October 26, 2015. It all started from a random conversation in our local digital watering hole, the [KopiJS](http://kopijs.org/) slack channel. The idea of starting a CSS meet-up seemed to take quite well and by 4pm we already had a logo, a GitHub organisation, a Twitter account and the venue for our inaugural meet-up.
+
+We can't talk about tech meet-ups in Singapore without mentioning [webuild.sg](https://webuild.sg/), *THE* hub for all things tech on this little red dot. If you're interested to attend any other tech meet-ups, definitely check it out. For data buffs, there is [https://data.webuild.sg/](https://data.webuild.sg/), which has information on tech meet-ups in Singapore dating back to December 2014.
---
+++ gulpfile.js
@@ -0,0 +1,95 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var sass        = require('gulp-sass');
+var prefix      = require('gulp-autoprefixer');
+var cssnano     = require('gulp-cssnano');
+var concat      = require('gulp-concat');
+var uglify      = require('gulp-uglify');
+var cp          = require('child_process');
+
+var messages = {
+    jekyllDev: '<span style="color: grey">Running:</span> $ jekyll build for dev',
+    jekyllProd: '<span style="color: grey">Running:</span> $ jekyll build for prod'
+};
+
+/**
+ * Build the Jekyll Site in development mode
+ */
+gulp.task('jekyll-dev', function (done) {
+  browserSync.notify(messages.jekyllDev);
+  return cp.spawn('jekyll', ['build', '--config', '_config.yml,_config_dev.yml'], {stdio: 'inherit'})
+    .on('close', done);
+});
+
+/**
+ * Rebuild Jekyll & do page reload
+ */
+gulp.task('jekyll-rebuild', ['jekyll-dev'], function () {
+  browserSync.reload();
+});
+
+/**
+ * Wait for jekyll-dev, then launch the Server
+ */
+gulp.task('browser-sync', ['sass', 'jekyll-dev'], function() {
+  browserSync.init({
+    server: "_site",
+    port: 2510
+  });
+});
+
+/**
+ * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
+ */
+gulp.task('sass', function () {
+  return gulp.src('_sass/styles.scss')
+    .pipe(sass({
+      includePaths: ['scss'],
+      onError: browserSync.notify
+    }))
+    .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+    .pipe(gulp.dest('_site/css'))
+    .pipe(browserSync.reload({stream:true}))
+    .pipe(gulp.dest('css'));
+});
+
+/**
+ * Watch scss files for changes & recompile
+ * Watch html/md files, run jekyll & reload BrowserSync
+ */
+gulp.task('watch', function () {
+  gulp.watch(['_sass/**/*.scss','_sass/*.scss'], ['sass']);
+  gulp.watch(['index.html', '_layouts/*.html', '_posts/*', '_includes/*.html', '*.md'], ['jekyll-rebuild']);
+});
+
+
+/**
+ * Build the Jekyll Site in production mode
+ */
+gulp.task('jekyll-prod', function (done) {
+  browserSync.notify(messages.jekyllProd);
+  return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
+    .on('close', done);
+});
+
+/**
+ * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
+ */
+gulp.task('sass-prod', function () {
+  return gulp.src('_sass/styles.scss')
+    .pipe(sass({
+      includePaths: ['scss'],
+      onError: browserSync.notify
+    }))
+    .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+    .pipe(cssnano())
+    .pipe(gulp.dest('_site/css'))
+    .pipe(gulp.dest('css'));
+});
+
+/**
+ * Default task, running just `gulp` will compile the sass,
+ * compile the jekyll site, launch BrowserSync & watch files.
+ */
+gulp.task('default', ['browser-sync', 'watch']);
+gulp.task('build', ['sass-prod', 'jekyll-prod']);
